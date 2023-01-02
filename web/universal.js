2bb8ec1e472418ae64866af1782b56be2303e747
Sidney Trzepacz <sidtrzisyourleader@gmail.com>
2023-01-01 20:11:33 -0500

    WebView

--- web/universal.js
+++ web/universal.js
@@ -1,4 +1,22 @@
-function navigate(url) {
+const constants = {
+    root: require('electron-root-path').rootPath
+}
+function navigate(url, file=true) {
     const webview = document.getElementById('mainwebview');
-    webview.src = url;
+    if (file) {
+        webview.loadURL("file://" + constants.root + "/web/" + url);
+    }
+    else {
+        webview.loadURL(url);
+    }
+    document.getElementById("webview-container").style.display = "block";
+}
+function exitApp() {
+    const webview = document.getElementById('mainwebview');
+    webview.loadURL("about:blank");
+    document.getElementById("webview-container").style.display = "none";
+}
+function refreshApp() {
+    const webview = document.getElementById('mainwebview');
+    webview.reload();
 }
